6d078e7760b7998de1a99761093d07011087dc26
Nenad Živanović <nen.zivanovic@gmail.com>
2019-05-29 20:42:19 +0200

    Add test that covers not excluding VCS ignored paths when configured that way

--- spec/fuzzy-finder-spec.js
+++ spec/fuzzy-finder-spec.js
@@ -1657,6 +1657,28 @@ describe('FuzzyFinder', () => {
           })
         })
 
+        describe('when core.excludeVcsIgnoredPaths is set to false', () => {
+          beforeEach(() => atom.config.set('core.excludeVcsIgnoredPaths', false))
+
+          describe("when the project's path is the repository's working directory", () => {
+            beforeEach(() => {
+              const ignoreFile = path.join(projectPath, '.gitignore')
+              fs.writeFileSync(ignoreFile, 'ignored.txt')
+
+              const ignoredFile = path.join(projectPath, 'ignored.txt')
+              fs.writeFileSync(ignoredFile, 'ignored text')
+            })
+
+            it("doesn't exclude paths that are git ignored", async () => {
+              await projectView.toggle()
+
+              await waitForPathsToDisplay(projectView)
+
+              expect(Array.from(projectView.element.querySelectorAll('li')).find(a => a.textContent.includes('ignored.txt'))).toBeDefined()
+            })
+          })
+        })
+
         describe('logging of metrics events', () => {
           let disposable
 
